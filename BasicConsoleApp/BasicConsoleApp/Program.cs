1506c5a65b0e46020a189a6dcac6748af8123855
Ritesh Moolya <riteshmoolya02@gmail.com>
2021-05-11 11:33:29 +0530

    First Commit

---
+++ BasicConsoleApp/BasicConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BasicConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Ritesh");
+        }
+    }
+}
