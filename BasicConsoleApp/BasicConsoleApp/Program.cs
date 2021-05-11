48d33cbb01b35348721c64e07510e2bd44e0d9a5
Ritesh Moolya <riteshmoolya02@gmail.com>
2021-05-11 12:24:29 +0530

    Revert "RiteshBranchModiefied"
    This reverts commit 16bd92832f24f877127d04833a0e50a29bc9ce61.

--- BasicConsoleApp/BasicConsoleApp/Program.cs
+++ BasicConsoleApp/BasicConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace BasicConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Ritesh");
+            Console.WriteLine("Hello World Ritesh");
         }
     }
 }
